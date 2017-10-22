3ea1a63859e6347f5f95fb7eae33b102e4cd78aa
TaoStarlit <zhengt2016@gmail.com>
2017-10-22 21:22:42 +1100

    rename and comments

--- chapter2/NaiveBayesModels-NewsTextClassification2.1.1.3.py
+++ chapter2/NaiveBayesModels-NewsTextClassification2.1.1.3.py
@@ -8,9 +8,9 @@ from sklearn.datasets import fetch_20newsgroups
 # 与之前预存的数据不同，fetch_20newsgroups需要即时从互联网下载数据。
 news = fetch_20newsgroups(subset='all')
 # 查验数据规模和细节。
-print len(news.data)
-print news.data[0]
-
+print "len(new.data): ", len(news.data)
+print "new.data[0]: ", news.data[0]
+print "new.target[0]: ", news.target[0]
 
 # In[2]:
 
@@ -18,7 +18,11 @@ print news.data[0]
 from sklearn.cross_validation import train_test_split
 # 随机采样25%的数据样本作为测试集。
 X_train, X_test, y_train, y_test = train_test_split(news.data, news.target, test_size=0.25, random_state=33)
-
+print "X_train: ", X_train[0]
+print "y_train: ", y_train[0]
+print "type(y_train): ", type(y_train)
+import numpy as np
+print "np.unique(y_train): ", np.unique(y_train, return_counts=True) #Find the unique elements of an array.
 
 # In[3]:
 
